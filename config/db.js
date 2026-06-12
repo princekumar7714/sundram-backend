27597f543f4a46973106ffc205f335b93c6cb4f6
prince kumar <princerajpit5868@gmail.com>
2026-06-12 13:51:36 -0400

    backend url updated

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log("MONGO_URI =", process.env.MONGO_URI);
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
